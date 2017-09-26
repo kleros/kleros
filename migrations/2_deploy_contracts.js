ecaf264e6fd1f66d184f31a633036c08600e74bc
Nicolas Wagner <contact@wagner-nicolas.com>
2017-09-26 17:07:38 +0200

    CS move to StandardJs

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,8 @@
-var ConvertLib = artifacts.require("./ConvertLib.sol");
-var MetaCoin = artifacts.require("./MetaCoin.sol");
+const ConvertLib = artifacts.require('./ConvertLib.sol')
+const MetaCoin = artifacts.require('./MetaCoin.sol')
 
 module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
-};
+  deployer.deploy(ConvertLib)
+  deployer.link(ConvertLib, MetaCoin)
+  deployer.deploy(MetaCoin)
+}
