bd9ad4426d3ba34de7a094acab5b9ea34dcf770b
clesaege <clemage@live.fr>
2017-10-06 19:05:12 +0200

    Implement proof of draws. Use packages. Explicit public and storage to silence warnings.

--- contracts/KlerosPOC.sol
+++ contracts/KlerosPOC.sol
@@ -6,7 +6,6 @@
 
 pragma solidity ^0.4.15;
 
-// We'll have to remove those for tests as truffle does not support github import.
 import "../../kleros-interaction/code/contracts/standard/arbitration/Arbitrator.sol";
 import "./Tokens/Token.sol";
 import "../../kleros-interaction//code/contracts/standard/rng/RNG.sol";
@@ -71,11 +70,13 @@ contract KlerosPOC is Arbitrator {
         uint appeals;              // Number of appeals.
         uint randomNumber;         // Random number drawn for the dispute to be use to draw jurors. Is 0 before the number is available.
         uint choices;              // The number of choices availables to the jurors.
+        uint initialNumberJurors;  // The initial number of jurors.
         DisputeState state;        // The state of the dispute.
         Vote[][] votes;            // The votes in the form vote[appeals][voteID].
         VoteCounter[] voteCounter; // The vote counters in the form voteCounter[appeals].
         mapping (address => uint) lastSessionVote; // Last session a juror has voted on this dispute. Is 0 if he never did.
     }
+    Dispute[] public disputes;
     
     /** @dev Constructor.
      *  @param _pinakion The address of the pinakion contract.
@@ -100,7 +101,7 @@ contract KlerosPOC is Arbitrator {
      *  @param _value Amount of fractions of token to deposit.
      */
     function deposit(address _from, uint _value) public onlyBy(pinakion) {
-        require(pinakion.transferFrom(msg.sender,this,_value));
+        require(pinakion.transferFrom(_from,this,_value));
         
         jurors[msg.sender].balance+=_value;
     }
@@ -110,7 +111,7 @@ contract KlerosPOC is Arbitrator {
      *  @param _value The amount to withdraw.
      */
     function withdraw(uint _value) public {
-        Juror juror = jurors[msg.sender];
+        Juror storage juror = jurors[msg.sender];
         require(juror.atStake<=juror.balance); // Make sure that there is no more at stake than owned to avoid overflow.
         require(_value<=juror.balance-juror.atStake);
         if (juror.lastSession==session)
@@ -122,13 +123,14 @@ contract KlerosPOC is Arbitrator {
     
     // **************************** //
     // *      Court functions     * //
+    // *    Modifying the state   * //
     // **************************** //
   
     /** @dev Activate tokens in order to have chances of being drawn. Note that once tokens are activated, there is no possibility of activating more.
      *  @param _value Amount of fractions of token to activate.
      */
-    function activateTokens(uint _value) onlyDuring(Period.Activation) {
-        Juror juror = jurors[msg.sender];
+    function activateTokens(uint _value) public onlyDuring(Period.Activation) {
+        Juror storage juror = jurors[msg.sender];
         require(_value<=juror.balance);
         require(_value>=minActivatedToken);
         require(juror.lastSession!=session); // Verify that tokens were not already activated for this session.
@@ -139,9 +141,50 @@ contract KlerosPOC is Arbitrator {
         juror.segmentEnd=segmentSize;
         
     }
-  
-  
-  
+    
+    // **************************** //
+    // *      Court functions     * //
+    // *     Constant and Pure    * //
+    // **************************** //  
+    
+    /** @dev Return the amount of jurors which are or will be drawn in the dispute.
+     *  The number of jurors is doubled and 1 is added at each appeal. We have proven the formula by recurrence.
+     *  This avoid having a variable number of jurors which would be updated in order to save gas.
+     *  @param _dispute The dispute we compute the amount of jurors.
+     *  @return nbJurors The number of jurors which are drawn.
+     */
+    function amountJurors(Dispute storage _dispute) internal constant returns(uint nbJurors) {
+        return (_dispute.initialNumberJurors+1) * 2**_dispute.appeals - 1;
+    }
+    
+    /** @dev Must be used to prove that a juror has been draw at least minWeight times.
+     *  We have to require the user to specify the draws that lead the juror to be drawn.
+     *  Because doing otherwise (looping throught all draws) could consume too much gas.
+     *  @param _jurorAddress Address of the juror we want to prove was drawn.
+     *  @param _disputeID The ID of the dispute the juror was drawn.
+     *  @param _draws The list of draws the juror was drawn. It draw numbering starts at 1 and the numbers should be increasing.
+     *  Note that in most cases this list will just contain 1 number.
+     */
+    function hasWeightAtMin(address _jurorAddress, uint _disputeID, uint[] _draws) public constant returns(uint minWeight) {
+        uint draw = 0;
+        Juror storage juror = jurors[_jurorAddress];
+        Dispute storage dispute = disputes[_disputeID];
+        uint nbJurors = amountJurors(dispute);
+        
+        require(juror.lastSession==session); // Make sure that the tokens were activated for this session.
+        require(period>Period.Draw); // Make sure that it's already drawn.
+        for (uint i;i<_draws.length;++i) {
+            require(_draws[i]>draw); // Make sure that draws are always increasing to avoid someone inputing the same multiple times.
+            draw = _draws[i];
+            require(draw<=nbJurors);
+            uint position = uint(keccak256(dispute.randomNumber,draw)) % segmentSize; // Random position on the segment for draw.
+            require(position>=juror.segmentStart);
+            require(position<juror.segmentEnd);
+        }
+        
+        return _draws.length;
+    }
+    
     
     
     
