71a7f127915efa7398f1ad4f51c3db5bdc4ba8c9
Clément Lesaege <clemage@live.fr>
2017-12-05 17:45:45 +0100

    TAB to spaces.

--- contracts/KlerosPOC.sol
+++ contracts/KlerosPOC.sol
@@ -395,7 +395,7 @@ contract KlerosPOC is Arbitrator {
         dispute.votes.length++;
         dispute.voteCounter.length++;
         
-		DisputeCreation(disputeID, Arbitrable(msg.sender));
+        DisputeCreation(disputeID, Arbitrable(msg.sender));
         return disputeID;
     }
     
@@ -404,7 +404,7 @@ contract KlerosPOC is Arbitrator {
      *  @param _extraData Standard but not used by this contract.
      */
     function appeal(uint _disputeID, bytes _extraData) public payable onlyDuring(Period.Appeal) {
-		super.appeal(_disputeID,_extraData);
+        super.appeal(_disputeID,_extraData);
         Dispute storage dispute = disputes[_disputeID];
         require(msg.value >= appealCost(_disputeID,_extraData));
         require(dispute.session+dispute.appeals == session); // Dispute of the current session.
