5ea7406e5d101c4c9bca59cf2f06c68ee3705694
Clément Lesaege <clemage@live.fr>
2018-05-29 19:53:12 +0000

    Fix spelling

--- contracts/KlerosPOC.sol
+++ contracts/KlerosPOC.sol
@@ -312,7 +312,7 @@ contract KlerosPOC is Arbitrator, ApproveAndCallFallBack, TokenController {
      *  Note that a juror who voted but without all his weight can't be penalized.
      *  @param _jurorAddress Address of the juror to steal tokens from.
      *  @param _disputeID The ID of the dispute the juror was drawn.
-     *  @param _draws The list of draws the juror was drawn. It draw numbering starts at 1 and the numbers should be increasing.
+     *  @param _draws The list of draws the juror was drawn. Numbering starts at 1 and the numbers should be increasing.
      */
     function penalizeInactiveJuror(address _jurorAddress, uint _disputeID, uint[] _draws) public {
         Dispute storage dispute = disputes[_disputeID];
