a4e3a249c5af3702aa6faee2f058999fbf3113c6
epiqueras <epiquerass@gmail.com>
2018-08-07 12:59:33 -0500

    feat(k-ary-sum-tree-factory): contract skeleton

---
+++ contracts/data-structures/KArySumTreeFactory.sol
@@ -0,0 +1,69 @@
+pragma solidity ^0.4.24;
+
+/**
+ *  @title KArySumTreeFactory
+ *  @author Enrique Piqueras - <epiquerass@gmail.com>
+ *  @dev A factory of k-ary sum trees.
+ */
+contract KArySumTreeFactory {
+    /* Enums */
+
+
+
+    /* Structs */
+
+
+
+    /* Events */
+
+
+
+    /* Storage */
+
+
+
+    /* Modifiers */
+
+
+
+    /* Constructor */
+
+
+
+    /* Fallback */
+
+
+
+    /* External */
+
+
+
+    /* External Views */
+
+
+
+    /* Public */
+
+
+
+    /* Public Views */
+
+
+
+    /* Internal */
+
+
+
+    /* Internal Views */
+
+
+
+    /* Private */
+
+
+
+    /* Private Views */
+
+
+
+}
