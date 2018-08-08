a81909935f8ecfba9012dee14173dda07ac93a47
epiqueras <epiquerass@gmail.com>
2018-08-08 16:53:14 -0500

    feat(k-ary-sum-tree-factory): create and delete trees

--- contracts/data-structures/KArySumTreeFactory.sol
+++ contracts/data-structures/KArySumTreeFactory.sol
@@ -6,63 +6,43 @@ pragma solidity ^0.4.24;
  *  @dev A factory of k-ary sum trees.
  */
 contract KArySumTreeFactory {
-    /* Enums */
-
-
-
     /* Structs */
 
-
-
-    /* Events */
-
-
+    struct KArySumTree {
+        uint K;
+        uint[] stack;
+        uint[] tree;
+    }
 
     /* Storage */
 
-
-
-    /* Modifiers */
-
-
-
-    /* Constructor */
-
-
-
-    /* Fallback */
-
-
-
-    /* External */
-
-
-
-    /* External Views */
-
-
-
-    /* Public */
-
-
-
-    /* Public Views */
-
-
+    mapping(bytes32 => KArySumTree) KArySumTrees;
 
     /* Internal */
 
+    /**
+     *  @dev Create a k-ary sum tree at the specified key.
+     *  @param _key The key of the new tree.
+     *  @param _K The number of children each node in the tree should have.
+     */
+    function createKArySumTree(bytes32 _key, uint _K) internal {
+        KArySumTrees[_key].K = _K;
+        KArySumTrees[_key].stack.length = 0;
+        KArySumTrees[_key].tree.length = 0;
+    }
 
+    /**
+     *  @dev Delete a k-ary sum tree at the specified key.
+     *  @param _key The key of the tree to delete.
+     */
+    function deleteKArySumTree(bytes32 _key) internal {
+        KArySumTrees[_key].K = 0;
+        KArySumTrees[_key].stack.length = 0;
+        KArySumTrees[_key].tree.length = 0;
+        delete KArySumTrees[_key];
+    }
 
     /* Internal Views */
-
-
-
-    /* Private */
-
-
-
-    /* Private Views */
 
 
 
