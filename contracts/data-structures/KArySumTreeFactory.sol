967a45437b1aad469f46ba749d19b991f037f1db
epiqueras <epiquerass@gmail.com>
2018-08-09 17:02:44 -0500

    feat(k-ary-sum-tree-factory): implement tests and fix bugs
    Closes #62, closes #71

--- contracts/data-structures/KArySumTreeFactory.sol
+++ contracts/data-structures/KArySumTreeFactory.sol
@@ -16,7 +16,7 @@ contract KArySumTreeFactory {
 
     /* Storage */
 
-    mapping(bytes32 => KArySumTree) KArySumTrees;
+    mapping(bytes32 => KArySumTree) internal kArySumTrees;
 
     /* Internal */
 
@@ -26,9 +26,9 @@ contract KArySumTreeFactory {
      *  @param _K The number of children each node in the tree should have.
      */
     function createKArySumTree(bytes32 _key, uint _K) internal {
-        KArySumTrees[_key].K = _K;
-        KArySumTrees[_key].stack.length = 0;
-        KArySumTrees[_key].tree.length = 0;
+        kArySumTrees[_key].K = _K;
+        kArySumTrees[_key].stack.length = 0;
+        kArySumTrees[_key].tree.length = 0;
     }
 
     /**
@@ -36,10 +36,10 @@ contract KArySumTreeFactory {
      *  @param _key The key of the tree to delete.
      */
     function deleteKArySumTree(bytes32 _key) internal {
-        KArySumTrees[_key].K = 0;
-        KArySumTrees[_key].stack.length = 0;
-        KArySumTrees[_key].tree.length = 0;
-        delete KArySumTrees[_key];
+        kArySumTrees[_key].K = 0;
+        kArySumTrees[_key].stack.length = 0;
+        kArySumTrees[_key].tree.length = 0;
+        delete kArySumTrees[_key];
     }
 
     /**
@@ -49,7 +49,7 @@ contract KArySumTreeFactory {
      *  @return The index of the appended value in the tree.
      */
     function append(bytes32 _key, uint _value) internal returns(uint treeIndex) {
-        KArySumTree storage tree = KArySumTrees[_key];
+        KArySumTree storage tree = kArySumTrees[_key];
 
         if (tree.stack.length == 0) { // No vacant spots
             // Get the index and append the value
@@ -58,7 +58,7 @@ contract KArySumTreeFactory {
             tree.tree[treeIndex] = _value;
 
             // Potentially append a new node and make the parent a sum node
-            if (treeIndex % tree.K == 0) { // Is first child
+            if ((treeIndex - 1) % tree.K == 0) { // Is first child
                 tree.tree.length++;
                 tree.tree[treeIndex + 1] = tree.tree[treeIndex / tree.K];
             }
@@ -71,10 +71,9 @@ contract KArySumTreeFactory {
 
         // Update parents
         uint parentIndex = treeIndex;
-        while (true) {
+        while (parentIndex != 0) {
             parentIndex = (parentIndex - 1) / tree.K;
             tree.tree[parentIndex] += _value;
-            if (parentIndex == 0) break;
         }
     }
 
@@ -84,7 +83,7 @@ contract KArySumTreeFactory {
      *  @param _treeIndex The index of the value to remove.
      */
     function remove(bytes32 _key, uint _treeIndex) internal {
-        KArySumTree storage tree = KArySumTrees[_key];
+        KArySumTree storage tree = kArySumTrees[_key];
 
         // Remember value and set to 0
         uint _value = tree.tree[_treeIndex];
@@ -96,10 +95,9 @@ contract KArySumTreeFactory {
 
         // Update parents
         uint parentIndex = _treeIndex;
-        while (true) {
+        while (parentIndex != 0) {
             parentIndex = (parentIndex - 1) / tree.K;
             tree.tree[parentIndex] -= _value;
-            if (parentIndex == 0) break;
         }
     }
 
@@ -113,7 +111,7 @@ contract KArySumTreeFactory {
      *  @return The index at which leafs start, the values of the returned leafs, and wether there are more for pagination.
      */
     function queryLeafs(bytes32 _key, uint _cursor, uint _count) internal view returns(uint startIndex, uint[] values, bool hasMore) {
-        KArySumTree storage tree = KArySumTrees[_key];
+        KArySumTree storage tree = kArySumTrees[_key];
 
         // Find the start index
         for (uint i = 0; i < tree.tree.length; i++) {
@@ -124,12 +122,13 @@ contract KArySumTreeFactory {
         }
 
         // Get the values
-        values = new uint[](_count);
-        uint _index = 0;
-        for (uint j = _cursor == 0 || startIndex > _cursor ? startIndex : _cursor + 1; j < tree.tree.length; j++) {
-            if (_index < _count) {
-                values[_index] = tree.tree[j];
-                _index++;
+        uint _startIndex = startIndex + _cursor;
+        values = new uint[](_startIndex + _count > tree.tree.length ? tree.tree.length - _startIndex : _count);
+        uint _valuesIndex = 0;
+        for (uint j = _startIndex; j < tree.tree.length; j++) {
+            if (_valuesIndex < _count) {
+                values[_valuesIndex] = tree.tree[j];
+                _valuesIndex++;
             } else {
                 hasMore = true;
                 break;
