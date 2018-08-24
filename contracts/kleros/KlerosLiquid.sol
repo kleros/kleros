86de45623db63e450e3c452a8bfd68534522378c
epiqueras <epiquerass@gmail.com>
2018-08-24 11:55:44 -0500

    feat(kleros-liquid): set up contract

---
+++ contracts/kleros/KlerosLiquid.sol
@@ -0,0 +1,73 @@
+pragma solidity ^0.4.24;
+
+import "kleros-interaction/contracts/standard/arbitration/Arbitrator.sol";
+
+import "../data-structures/SortitionSumTreeFactory.sol";
+
+/**
+ *  @title KlerosLiquid
+ *  @author Enrique Piqueras - <epiquerass@gmail.com>
+ *  @dev The main Kleros contract with dispute resolution logic for the Athena release.
+ */
+contract KlerosLiquid is SortitionSumTreeFactory, Arbitrator {
+    /* Enums */
+
+
+
+    /* Structs */
+
+
+
+    /* Events */
+
+
+
+    /* Storage */
+
+
+
+    /* Modifiers */
+
+
+
+    /* Constructor */
+
+
+
+    /* Fallback */
+
+
+
+    /* External */
+
+
+
+    /* External Views */
+
+
+
+    /* Public */
+
+
+
+    /* Public Views */
+
+
+
+    /* Internal */
+
+
+
+    /* Internal Views */
+
+
+
+    /* Private */
+
+
+
+    /* Private Views */
+
+
+
+}
