197d3a5760aa01b6fee0dbc9b92cd60e2fd020fb
Ferit Tunçer <ferit@cryptolab.net>
2018-09-05 15:53:00 +0300

    feat(governance): add governance contract

---
+++ contracts/Governance.sol
@@ -0,0 +1,271 @@
+pragma solidity ^0.4.24;
+
+import "kleros-interaction/contracts/standard/permission/ArbitrablePermissionList.sol";
+import "kleros-interaction/contracts/standard/arbitration/CentralizedArbitrator.sol"; // I need this contract to be deployed for tests, Truffle issue
+import { ApproveAndCallFallBack, MiniMeToken, MiniMeTokenFactory, TokenController } from "minimetoken/contracts/MiniMeToken.sol";
+import { MiniMeTokenERC20 as Pinakion } from "kleros-interaction/contracts/standard/arbitration/ArbitrableTokens/MiniMeTokenERC20.sol";
+
+contract Governance {
+
+    Pinakion public pinakion;
+    TokenController public tokenController;
+    ArbitrablePermissionList public proposalList;
+
+    uint public proposalQuorum;
+    uint public currentProposalQuorum;
+
+    uint public quorumDivideTime;
+    uint public lastTimeQuorumReached;
+
+    uint public votingTime;
+    uint public currentVotingTime;
+
+    string constant voteTokenName = "Vote Token";
+    string constant quorumTokenName = "Quorum Token";
+    string constant voteTokenSymbol = "VOTE";
+    string constant quorumTokenSymbol = "QUORUM";
+    uint8 constant DECIMALS = 18;
+    address public constant supportDeposit = 0x707574546F566F74650000000000000000000000; // When this address reaches quorum proposal gets put to vote.
+    address public constant approvalDeposit =  0x617070726f76616c000000000000000000000000; // This address represents yes votes.
+    address public constant rejectionDeposit = 0x72656a656374696F6E0000000000000000000000; // This address represents no votes.
+
+    enum ProposalState {
+        New,
+        PutToSupport,
+        PutToVote,
+        Decided
+    }
+
+    struct Proposal {
+        address destination; // The governed contract.
+        uint amount; // The amount of ETH to send (in most cases it should be 0).
+        bytes data; // The data (similar to a multisig mechanism).
+        string uriDescription; // URI to a natural language description of the proposal. Also used as ID in mappings.
+        bytes32 descriptionHash; // Hash of the description.
+        string uriArguments; // URI to arguments for the proposal.
+        bytes32 argumentsHash; // Hash of the arguments.
+        ProposalState state; // State of proposal.
+        uint whenPutToVote; // Records the time when a proposal put to vote to be able to calculate voting period.
+        MiniMeToken quorumToken; // The token that will be used for quorum.
+        MiniMeToken voteToken; // The token that will be used for actual proposal voting.
+        bool approved; // Outcome of voting.
+    }
+
+    mapping(bytes32 => Proposal) public proposals;
+
+
+    constructor (uint _proposalQuorum, uint _quorumDivideTime, uint _votingTime, ArbitrablePermissionList _arbitrablePermissionList, Pinakion _pinakion, TokenController _tokenController) public {
+        lastTimeQuorumReached = block.timestamp;
+
+        proposalList = _arbitrablePermissionList;
+        pinakion = _pinakion;
+        tokenController = _tokenController;
+
+        proposalQuorum = _proposalQuorum;
+        currentProposalQuorum = proposalQuorum;
+
+        quorumDivideTime = _quorumDivideTime;
+        votingTime = _votingTime;
+    }
+
+
+    // ****************************** //
+    // *          Modifiers         * //
+    // ****************************** //
+
+    modifier onlyItself() {
+        require(msg.sender == address(this), "Only the contract itself can call this.");
+        _;
+    }
+
+    modifier onlyWhenProposalInNew(bytes32 _id) {
+        require(proposals[_id].state == ProposalState.New, "Only when proposal in New state.");
+        _;
+    }
+
+    modifier onlyWhenProposalPutToSupport(bytes32 _id) {
+        require(proposals[_id].state == ProposalState.PutToSupport, "Only when proposal in PutToSupport state.");
+        _;
+    }
+
+    modifier onlyWhenProposalPutToVote(bytes32 _id) {
+        require(proposals[_id].state == ProposalState.PutToVote, "Only when proposal in PutToVote state.");
+        _;
+    }
+
+    modifier onlyWhenProposalIsDecided(bytes32 _id) {
+        require(proposals[_id].state == ProposalState.Decided, "Only when proposal in Decided state.");
+        _;
+    }
+
+    modifier onlyWhenVotingPeriod(bytes32 _id) {
+        require(proposals[_id].state == ProposalState.PutToVote, "Only when proposal in PutToVote state.");
+        require(block.timestamp <= proposals[_id].whenPutToVote + votingTime, "Only when voting period.");
+        _;
+    }
+
+    modifier onlyWhenPermitted(bytes32 _id) {
+        require(proposalList.isPermitted(_id), "Only when proposal is permitted.");
+        _;
+    }
+
+    modifier onlyWhenQuorumReached(bytes32 _id) {
+        require(proposals[_id].quorumToken.balanceOf(supportDeposit) > getRequiredQuorum(_id), "Only when propsal has quorum.");
+        _;
+    }
+
+    modifier onlyWhenVotingPeriodPassed(bytes32 _id) {
+        require(block.timestamp > proposals[_id].whenPutToVote + votingTime, "Only when voting period passed.");
+        _;
+    }
+
+    modifier onlyWhenApproved(bytes32 _id) {
+        require(proposals[_id].approved, "Proposal is not approved.");
+        _;
+    }
+
+
+    // ****************************** //
+    // *           Events           * //
+    // ****************************** //
+
+    event ProposalPutToSupport(bytes32 indexed _id);
+
+    event ProposalPutToVote(bytes32 indexed _id);
+
+    event ProposalDecided(bytes32 indexed _id, bool approved);
+
+
+    // ****************************** //
+    // *    Governance Mechanism    * //
+    // ****************************** //
+
+    /** @dev Creates a proposal and adds to ProposalState
+     *  @param _id ID of the proposalList
+     *  @param _destination Destination contract of the execution
+     *  @param _amount Value of the execution
+     *  @param _data Data of the execution
+     *  @param _uriDescription URI of the description of the proposal
+     *  @param _uriArguments URI of the arguments of the proposal
+     */
+    function createProposal(bytes32 _id, address _destination, uint _amount, bytes _data, string _uriDescription, string _uriArguments) public {
+        require(proposals[_id].destination == address(0), "There is already a proposal with this id");
+
+        proposals[_id].destination = _destination;
+        proposals[_id].amount = _amount;
+        proposals[_id].data = _data;
+        proposals[_id].uriDescription = _uriDescription;
+        proposals[_id].uriArguments = _uriArguments;
+    }
+
+    /** @dev Request registering a proposal to the proposal list
+     *  @param _id ID of a proposal
+     */
+    function requestRegisteringProposal(bytes32 _id) public payable onlyWhenProposalInNew(_id) {
+        proposalList.requestRegistration.value(msg.value)(_id);
+    }
+
+    /** @dev Put proposal to support voting only when a new proposal is permitted.
+     *  @param _id ID of a proposal
+     */
+    function putProposalToSupport(bytes32 _id) public onlyWhenProposalInNew(_id) onlyWhenPermitted(_id) {
+        Proposal storage proposal = proposals[_id];
+
+        address cloneToken = pinakion.createCloneToken({_cloneTokenName: quorumTokenName, _cloneDecimalUnits: DECIMALS, _cloneTokenSymbol: quorumTokenSymbol, _snapshotBlock: block.number, _transfersEnabled: true});
+        proposal.quorumToken = MiniMeToken(cloneToken);
+        proposal.quorumToken.changeController(tokenController);
+
+        proposal.state = ProposalState.PutToSupport;
+
+        emit ProposalPutToSupport(_id);
+    }
+
+
+    /** @dev Calculate and return required quorum for a given proposal.
+     *  @param _id ID of a proposal
+     */
+    function getRequiredQuorum(bytes32 _id) public view onlyWhenProposalPutToSupport(_id) returns (uint effectiveQuorum){ // Maybe use fixed point numbers here
+        uint numberOfDividePeriodsPassed = (block.timestamp - lastTimeQuorumReached) / quorumDivideTime;
+        uint effectiveQuorumPercentage = currentProposalQuorum / (2 ** numberOfDividePeriodsPassed);
+        effectiveQuorum = effectiveQuorumPercentage * proposals[_id].quorumToken.totalSupply() / 100;
+    }
+
+
+    /** @dev Put given proposal to vote.
+     *  @param _id ID of a proposal
+     */
+    function putProposalToVote(bytes32 _id) public onlyWhenProposalPutToSupport(_id) onlyWhenQuorumReached(_id) {
+
+        Proposal storage proposal = proposals[_id];
+
+        proposal.whenPutToVote = block.timestamp;
+
+        address cloneToken = pinakion.createCloneToken({_cloneTokenName: voteTokenName, _cloneDecimalUnits: DECIMALS, _cloneTokenSymbol: voteTokenSymbol, _snapshotBlock: block.number, _transfersEnabled: true});
+        proposal.voteToken = MiniMeToken(cloneToken);
+        proposal.voteToken.changeController(tokenController);
+
+
+        proposal.state = ProposalState.PutToVote;
+
+        emit ProposalPutToVote(_id);
+
+        resetSettings(); // Reset global variables as quorum has reached
+    }
+
+
+    /** @dev Ends a voting, moves proposal to decided state, sets the decision
+     *  @param _id ID of a proposal
+     */
+    function finalizeVoting(bytes32 _id) public onlyWhenProposalPutToVote(_id) onlyWhenVotingPeriodPassed(_id) {
+        proposals[_id].state = ProposalState.Decided;
+        proposals[_id].approved = proposals[_id].voteToken.balanceOf(approvalDeposit) > proposals[_id].voteToken.balanceOf(rejectionDeposit);
+
+        emit ProposalDecided(_id, proposals[_id].approved);
+    }
+
+    /** @dev General purpose call function for executing a proposal UNTRUSTED
+     *  @param _id ID of a proposal
+     */
+    function executeProposal(bytes32 _id) public onlyWhenApproved(_id) {
+        Proposal storage proposal = proposals[_id];
+
+        proposal.destination.call.value(proposal.amount)(proposal.data); // solium-disable-line security/no-call-value
+    }
+
+
+    /** @dev Makes necessary resets when a proposal reaches quorumToken
+     */
+    function resetSettings() internal {
+        lastTimeQuorumReached = block.timestamp; // Necessary when calculating required quorum as it is halved periodically.
+        currentProposalQuorum = proposalQuorum; // Update required quorum percent, which will be constant during new quorum phase.
+        currentVotingTime = votingTime; // Update allowed voting time, which will be constant during new quorum phase.
+    }
+
+
+    // ***************** //
+    // *    Setters    * //
+    // ***************** //
+
+    /** @dev Setter for proposalQuorum
+     *  @param _proposalQuorum Value to be set.
+     */
+    function setProposalQuorum(uint _proposalQuorum) public onlyItself {
+        proposalQuorum = _proposalQuorum;
+    }
+
+    /** @dev Setter for votingTime
+     *  @param _votingTime Value to be set.
+     */
+    function setVotingTime(uint _votingTime) public onlyItself {
+        votingTime = _votingTime;
+    }
+
+    /** @dev Setter for quorumDivideTime
+     *  @param _quorumDivideTime Value to be set.
+     */
+    function setQuorumDivideTime(uint _quorumDivideTime) public onlyItself {
+        quorumDivideTime = _quorumDivideTime;
+    }
+
+
+}
---
+++ test/governance.js
@@ -0,0 +1,468 @@
+/* eslint-disable no-undef */ // Avoid the linter considering truffle elements as undef.
+
+const BigNumber = web3.BigNumber
+const {
+  expectThrow
+} = require('openzeppelin-solidity/test/helpers/expectThrow')
+const {
+  increaseTime
+} = require('openzeppelin-solidity/test/helpers/increaseTime')
+
+const GOVERNANCE = artifacts.require('Governance')
+const ARBITRABLE_PERMISSION_LIST = artifacts.require('ArbitrablePermissionList')
+const CENTRALIZED_ARBITRATOR = artifacts.require('CentralizedArbitrator')
+const MINIME_TOKEN = artifacts.require('MiniMeToken')
+const MINIME_TOKEN_FACTORY = artifacts.require('MiniMeTokenFactory')
+const KLEROS_LIQUID = artifacts.require('KlerosLiquid')
+const ConstantNG = artifacts.require('ConstantNG')
+
+contract('Governance', function(accounts) {
+  const PROPOSAL_QUORUM = 60
+  const QUORUM_DIVIDE_TIME = 100
+  const VOTING_TIME = 1000
+
+  const CREATOR = accounts[1]
+  const CREATOR_EXTRA_DATA = '0x707574546F566F74650000000000000000000000'
+  const ARBITRATION_FEE = 4
+  const STAKE = 10
+  const TIME_TO_CHALLENGE = 0
+  const META_EVIDENCE = 'evidence'
+  const BLACKLIST = false
+  const APPEND_ONLY = true
+  const RECHALLENGE_POSSIBLE = false
+
+  const MIN_STAKING_TIME = 1
+  const MAX_DRAWINNG_TIME = 1
+
+  let governance
+  let arbitrablePermissionList
+  let centralizedArbitrator
+  let pinakion
+  let tokenController
+  let tokenFactory
+  let RNG
+
+  const ARBITRARY_STRING = 'ARBITRARY_STRING'
+
+  const ITEM_STATUS = {
+    ABSENT: 0,
+    CLEARED: 1,
+    RESUBMITTED: 2,
+    REGISTERED: 3,
+    SUBMITTED: 4,
+    CLEARING_REQUESTED: 5,
+    PREVENTIVE_CLEARING_REQUESTED: 6
+  }
+
+  const PROPOSAL_STATE = {
+    NEW: 0,
+    PUT_TO_SUPPORT: 1,
+    PUT_TO_VOTE: 2,
+    DECIDED: 3
+  }
+
+  // Helpers
+  const randomInt = (max, min = 1) =>
+    Math.max(min, Math.ceil(Math.random() * max))
+  const generateSubcourts = (
+    K,
+    depth,
+    ID = 0,
+    minStake = 0,
+    subcourtMap = {}
+  ) => {
+    const newMinStake = Math.max(randomInt(100), minStake)
+    const subcourtTree = {
+      ID,
+      hiddenVotes: ID % 2 === 0,
+      minStake: newMinStake,
+      alpha: randomInt(1000),
+      jurorFee: randomInt(100),
+      minJurors: randomInt(5, 3),
+      jurorsForJump: randomInt(15, 3),
+      timesPerPeriod: [...new Array(4)].map(_ => randomInt(5)),
+      sortitionSumTreeK: randomInt(5),
+      children:
+        depth > 1
+          ? [...new Array(K)].map(
+              (_, i) =>
+                generateSubcourts(
+                  K,
+                  depth - 1,
+                  K * ID + i + 1,
+                  newMinStake,
+                  subcourtMap
+                ).subcourtTree
+            )
+          : undefined
+    }
+    if (ID === 0) subcourtTree.parent = 0
+    else {
+      subcourtTree.parent = Math.floor((ID - 1) / K)
+      subcourtMap[subcourtTree.ID] = {
+        ...subcourtTree,
+        children:
+          subcourtTree.children && subcourtTree.children.map(child => child.ID)
+      }
+    }
+    return { subcourtTree, subcourtMap }
+  }
+
+  beforeEach('setup contract for each test', async function() {
+    centralizedArbitrator = await CENTRALIZED_ARBITRATOR.new(ARBITRATION_FEE, {
+      from: CREATOR
+    })
+
+    arbitrablePermissionList = await ARBITRABLE_PERMISSION_LIST.new(
+      centralizedArbitrator.address,
+      CREATOR_EXTRA_DATA,
+      META_EVIDENCE,
+      BLACKLIST,
+      APPEND_ONLY,
+      RECHALLENGE_POSSIBLE,
+      STAKE,
+      TIME_TO_CHALLENGE,
+      {
+        from: CREATOR
+      }
+    )
+
+    tokenFactory = await MINIME_TOKEN_FACTORY.new({ from: CREATOR })
+
+    pinakion = await MINIME_TOKEN.new(
+      tokenFactory.address,
+      0x0,
+      0,
+      'Pinakion',
+      18,
+      'PNK',
+      true,
+      {
+        from: CREATOR
+      }
+    )
+
+    pinakion.generateTokens(
+      CREATOR,
+      (await web3.eth.getBalance(CREATOR)).toNumber(),
+      { from: CREATOR }
+    )
+
+    const RANDOM_NUMBER = 10
+    RNG = await ConstantNG.new(RANDOM_NUMBER)
+
+    const { subcourtTree } = generateSubcourts(randomInt(4, 2), 3)
+
+    tokenController = await KLEROS_LIQUID.new(
+      CREATOR,
+      pinakion.address,
+      RNG.address,
+      MIN_STAKING_TIME,
+      MAX_DRAWINNG_TIME,
+      subcourtTree.hiddenVotes,
+      subcourtTree.minStake,
+      subcourtTree.alpha,
+      subcourtTree.jurorFee,
+      subcourtTree.minJurors,
+      subcourtTree.jurorsForJump,
+      subcourtTree.timesPerPeriod,
+      subcourtTree.sortitionSumTreeK
+    )
+
+    await pinakion.changeController(tokenController.address, { from: CREATOR })
+
+    assert.equal(await pinakion.controller(), tokenController.address) // Make sure that kleros liquid contract is the controller of the pinakion token
+
+    governance = await GOVERNANCE.new(
+      PROPOSAL_QUORUM,
+      QUORUM_DIVIDE_TIME,
+      VOTING_TIME,
+      arbitrablePermissionList.address,
+      pinakion.address,
+      tokenController.address,
+      { from: CREATOR }
+    )
+
+    await tokenController.changeGovernor(governance.address, { from: CREATOR })
+
+    assert.equal(await tokenController.governor(), governance.address) // Make sure that governance contract is the governor of kleros liquid contract
+  })
+
+  it('should not let to set proposal quorum time other than itself(governor)', async function() {
+    await expectThrow(governance.setProposalQuorum(0, { from: CREATOR }))
+  })
+
+  it('should not let to set voting time other than itself(governor)', async function() {
+    await expectThrow(governance.setVotingTime(0, { from: CREATOR }))
+  })
+
+  it('should not let to set quorum divide time other than itself(governor)', async function() {
+    await expectThrow(governance.setQuorumDivideTime(0, { from: CREATOR }))
+  })
+
+  it('should request registration of a proposal to the proposal list', async function() {
+    await governance.requestRegisteringProposal(ARBITRARY_STRING, {
+      from: accounts[3],
+      value: 10000000
+    })
+
+    const PROPOSAL_LIST_ADDRESS = await governance.proposalList()
+    const arbitrablePermissionList = web3.eth.contract(
+      ARBITRABLE_PERMISSION_LIST.abi
+    )
+    const PROPOSAL_LIST = arbitrablePermissionList.at(PROPOSAL_LIST_ADDRESS)
+
+    const ACTUAL = (await PROPOSAL_LIST.items(ARBITRARY_STRING))[0].toNumber()
+    const EXPECTED = ITEM_STATUS.SUBMITTED
+    assert.equal(ACTUAL, EXPECTED)
+  })
+
+  it('should put proposal to support', async function() {
+    await governance.requestRegisteringProposal(ARBITRARY_STRING, {
+      from: accounts[3],
+      value: 10000000
+    })
+
+    await governance.putProposalToSupport(ARBITRARY_STRING, {
+      from: CREATOR,
+      gas: 3000000
+    })
+
+    const ACTUAL = (await governance.proposals(ARBITRARY_STRING))[7].toNumber()
+    const EXPECTED = PROPOSAL_STATE.PUT_TO_SUPPORT
+    assert.equal(ACTUAL, EXPECTED)
+  })
+
+  it('should be possible support a proposal', async function() {
+    await governance.requestRegisteringProposal(ARBITRARY_STRING, {
+      from: accounts[3],
+      value: 10000000
+    })
+
+    await governance.putProposalToSupport(ARBITRARY_STRING, {
+      from: CREATOR,
+      gas: 3000000
+    })
+
+    const QUORUM_TOKEN_ADDRESS = (await governance.proposals(
+      ARBITRARY_STRING
+    ))[9]
+    const miniMeToken = web3.eth.contract(MINIME_TOKEN.abi)
+    const QUORUM_TOKEN = miniMeToken.at(QUORUM_TOKEN_ADDRESS)
+
+    const DEPOSIT_ADDRESS = await governance.supportDeposit()
+    const TRANSFER_AMOUNT = await QUORUM_TOKEN.balanceOf(CREATOR)
+    await QUORUM_TOKEN.transfer(DEPOSIT_ADDRESS, TRANSFER_AMOUNT, {
+      from: CREATOR,
+      gas: 3000000
+    })
+
+    const ACTUAL = QUORUM_TOKEN.balanceOf(DEPOSIT_ADDRESS).toNumber()
+    const EXPECTED = TRANSFER_AMOUNT
+    assert.equal(ACTUAL, EXPECTED)
+  })
+
+  it('should getRequiredQuorum', async function() {
+    await governance.requestRegisteringProposal(ARBITRARY_STRING, {
+      from: accounts[3],
+      value: 10000000
+    })
+
+    await governance.putProposalToSupport(ARBITRARY_STRING, {
+      from: CREATOR,
+      gas: 3000000
+    })
+
+    const ACTUAL = new BigNumber(
+      await governance.getRequiredQuorum(ARBITRARY_STRING, {
+        from: CREATOR
+      })
+    )
+    const QUORUM_TOKEN_ADDRESS = (await governance.proposals(
+      ARBITRARY_STRING
+    ))[9]
+    const miniMeToken = web3.eth.contract(MINIME_TOKEN.abi)
+    const QUORUM_TOKEN = miniMeToken.at(QUORUM_TOKEN_ADDRESS)
+    const EXPECTED = new BigNumber(await QUORUM_TOKEN.totalSupply())
+      .div(100)
+      .mul(PROPOSAL_QUORUM)
+    assert(ACTUAL.equals(EXPECTED))
+  })
+
+  it('should be possible to put a proposal to vote', async function() {
+    // TODO ASSERTS
+    await governance.requestRegisteringProposal(ARBITRARY_STRING, {
+      from: accounts[3],
+      value: 10000000
+    })
+
+    await governance.putProposalToSupport(ARBITRARY_STRING, {
+      from: CREATOR,
+      gas: 3000000
+    })
+
+    const QUORUM_TOKEN_ADDRESS = (await governance.proposals(
+      ARBITRARY_STRING
+    ))[9]
+    const miniMeToken = web3.eth.contract(MINIME_TOKEN.abi)
+    const QUORUM_TOKEN = miniMeToken.at(QUORUM_TOKEN_ADDRESS)
+    const DEPOSIT_ADDRESS = await governance.supportDeposit()
+    const TRANSFER_AMOUNT = await QUORUM_TOKEN.balanceOf(CREATOR)
+    await QUORUM_TOKEN.transfer(DEPOSIT_ADDRESS, TRANSFER_AMOUNT, {
+      from: CREATOR,
+      gas: 3000000
+    })
+
+    await governance.putProposalToVote(ARBITRARY_STRING, {
+      from: CREATOR,
+      gas: 3000000
+    })
+
+    const ACTUAL = (await governance.proposals(ARBITRARY_STRING))[7].toNumber()
+    const EXPECTED = PROPOSAL_STATE.PUT_TO_VOTE
+    assert.equal(ACTUAL, EXPECTED)
+  })
+
+  it('should be possible to vote a proposal', async function() {
+    await governance.requestRegisteringProposal(ARBITRARY_STRING, {
+      from: accounts[3],
+      value: 10000000
+    })
+
+    await governance.putProposalToSupport(ARBITRARY_STRING, {
+      from: CREATOR,
+      gas: 3000000
+    })
+
+    const QUORUM_TOKEN_ADDRESS = (await governance.proposals(
+      ARBITRARY_STRING
+    ))[9]
+    const miniMeToken = web3.eth.contract(MINIME_TOKEN.abi)
+    const QUORUM_TOKEN = miniMeToken.at(QUORUM_TOKEN_ADDRESS)
+    let DEPOSIT_ADDRESS = await governance.supportDeposit()
+    let TRANSFER_AMOUNT = await QUORUM_TOKEN.balanceOf(CREATOR)
+    await QUORUM_TOKEN.transfer(DEPOSIT_ADDRESS, TRANSFER_AMOUNT, {
+      from: CREATOR,
+      gas: 3000000
+    })
+
+    await governance.putProposalToVote(ARBITRARY_STRING, {
+      from: CREATOR,
+      gas: 3000000
+    })
+
+    const VOTE_TOKEN_ADDRESS = (await governance.proposals(
+      ARBITRARY_STRING
+    ))[10]
+    const VOTE_TOKEN = miniMeToken.at(VOTE_TOKEN_ADDRESS)
+    DEPOSIT_ADDRESS = await governance.approvalDeposit()
+    TRANSFER_AMOUNT = await VOTE_TOKEN.balanceOf(CREATOR)
+    await VOTE_TOKEN.transfer(DEPOSIT_ADDRESS, TRANSFER_AMOUNT, {
+      from: CREATOR,
+      gas: 3000000
+    })
+
+    const ACTUAL = VOTE_TOKEN.balanceOf(DEPOSIT_ADDRESS).toNumber()
+    const EXPECTED = TRANSFER_AMOUNT
+    assert.equal(ACTUAL, EXPECTED)
+  })
+
+  it('should be possible to finalize a voting', async function() {
+    await governance.requestRegisteringProposal(ARBITRARY_STRING, {
+      from: accounts[3],
+      value: 10000000
+    })
+
+    await governance.putProposalToSupport(ARBITRARY_STRING, {
+      from: CREATOR,
+      gas: 3000000
+    })
+
+    const QUORUM_TOKEN_ADDRESS = (await governance.proposals(
+      ARBITRARY_STRING
+    ))[9]
+    const miniMeToken = web3.eth.contract(MINIME_TOKEN.abi)
+    const QUORUM_TOKEN = miniMeToken.at(QUORUM_TOKEN_ADDRESS)
+    let DEPOSIT_ADDRESS = await governance.supportDeposit()
+    let TRANSFER_AMOUNT = await QUORUM_TOKEN.balanceOf(CREATOR)
+    await QUORUM_TOKEN.transfer(DEPOSIT_ADDRESS, TRANSFER_AMOUNT, {
+      from: CREATOR,
+      gas: 3000000
+    })
+
+    await governance.putProposalToVote(ARBITRARY_STRING, {
+      from: CREATOR,
+      gas: 3000000
+    })
+
+    const VOTE_TOKEN_ADDRESS = (await governance.proposals(
+      ARBITRARY_STRING
+    ))[10]
+    const VOTE_TOKEN = miniMeToken.at(VOTE_TOKEN_ADDRESS)
+    DEPOSIT_ADDRESS = await governance.approvalDeposit()
+    TRANSFER_AMOUNT = await VOTE_TOKEN.balanceOf(CREATOR)
+    await VOTE_TOKEN.transfer(DEPOSIT_ADDRESS, TRANSFER_AMOUNT, {
+      from: CREATOR,
+      gas: 3000000
+    })
+
+    await increaseTime(2000)
+
+    await governance.finalizeVoting(ARBITRARY_STRING)
+
+    const ACTUAL = (await governance.proposals(ARBITRARY_STRING))[7].toNumber()
+    const EXPECTED = PROPOSAL_STATE.DECIDED
+    assert.equal(ACTUAL, EXPECTED)
+  })
+
+  // For this test I need web3 1.0.0-beta36 for encoding function call.
+  //   it('should be possible to execute a proposal', async function() {
+  //     const DESTINATION = governance.address
+  //     const AMOUNT = 0
+  //     console.log(web3)
+  //     const DATA = web3.eth.abi.encodeFunctionCall({
+  //     name: 'setVotingTime',
+  //     type: 'function',
+  //     inputs: [{
+  //         type: 'uint256',
+  //         name: '_votingTime'
+  //     }]
+  // }, ['3000']);
+  //
+  //     const URI_DESCRIPTION = "description"
+  //     const URI_ARGUMENTS = "arguments"
+  //
+  //     await governance.createProposal(ARBITRARY_STRING, )
+  //
+  //     await governance.requestRegisteringProposal(ARBITRARY_STRING, {
+  //       from: accounts[3], value: 10000000
+  //     })
+  //
+  //     await governance.putProposalToSupport(ARBITRARY_STRING, {
+  //         from: CREATOR, gas: 3000000
+  //     })
+  //
+  //     const QUORUM_TOKEN_ADDRESS = (await governance.proposals(ARBITRARY_STRING))[9]
+  //     let miniMeToken = web3.eth.contract(MINIME_TOKEN.abi)
+  //     const QUORUM_TOKEN = miniMeToken.at(QUORUM_TOKEN_ADDRESS)
+  //     let DEPOSIT_ADDRESS = await governance.supportDeposit()
+  //     let TRANSFER_AMOUNT = await QUORUM_TOKEN.balanceOf(CREATOR)
+  //     await QUORUM_TOKEN.transfer(DEPOSIT_ADDRESS, TRANSFER_AMOUNT, {from: CREATOR, gas: 3000000})
+  //
+  //     await governance.putProposalToVote(ARBITRARY_STRING, {
+  //       from: CREATOR, gas: 3000000
+  //     })
+  //
+  //     const VOTE_TOKEN_ADDRESS = (await governance.proposals(ARBITRARY_STRING))[10]
+  //     const VOTE_TOKEN = miniMeToken.at(QUORUM_TOKEN_ADDRESS)
+  //     DEPOSIT_ADDRESS = await governance.approvalDeposit()
+  //     TRANSFER_AMOUNT = await VOTE_TOKEN.balanceOf(CREATOR)
+  //     await VOTE_TOKEN.transfer(DEPOSIT_ADDRESS, TRANSFER_AMOUNT, {from: CREATOR, gas: 3000000})
+  //
+  //     await increaseTime(2000)
+  //
+  //     await governance.finalizeVoting(ARBITRARY_STRING)
+  //
+  //     await governance.executeProposal(ARBITRARY_STRING)
+  //   })
+})
