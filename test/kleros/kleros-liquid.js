68688c3f9d6190fbeffe2dcfc87023f0ac2cebf7
Ferit Tunçer <ferit@cryptolab.net>
2018-10-01 15:52:50 +0300

    fix(helpers): add missing await

--- test/kleros/kleros-liquid.js
+++ test/kleros/kleros-liquid.js
@@ -344,7 +344,7 @@ contract('KlerosLiquid', accounts =>
         const subcourt = subcourtMap[dispute.subcourtID] || subcourtTree
 
         // Generate random number
-        increaseTime(minStakingTime)
+        await increaseTime(minStakingTime)
         await klerosLiquid.passPhase()
         await klerosLiquid.passPhase()
 
@@ -359,7 +359,7 @@ contract('KlerosLiquid', accounts =>
           )).length
         )
         totalJurorFees.push(subcourt.jurorFee * numberOfDraws[i])
-        increaseTime(subcourt.timesPerPeriod[0])
+        await increaseTime(subcourt.timesPerPeriod[0])
         await klerosLiquid.passPeriod(dispute.ID)
 
         // Decide votes
@@ -381,14 +381,14 @@ contract('KlerosLiquid', accounts =>
               i,
               soliditySha3(dispute.ID, i, votes[i], i)
             )
-          increaseTime(subcourt.timesPerPeriod[1])
+          await increaseTime(subcourt.timesPerPeriod[1])
           await klerosLiquid.passPeriod(dispute.ID)
         }
 
         // Vote
         for (let i = 0; i < votes.length; i++)
           await klerosLiquid.vote(dispute.ID, i, votes[i], i)
-        increaseTime(subcourt.timesPerPeriod[2])
+        await increaseTime(subcourt.timesPerPeriod[2])
         await klerosLiquid.passPeriod(dispute.ID)
 
         // Appeal or execute
@@ -407,7 +407,7 @@ contract('KlerosLiquid', accounts =>
             dispute.ID
           ))[0].toNumber()
         } else {
-          increaseTime(subcourt.timesPerPeriod[3])
+          await increaseTime(subcourt.timesPerPeriod[3])
           await klerosLiquid.passPeriod(dispute.ID)
           for (let i = 0; i <= dispute.appeals; i++) {
             const PNKBefore = await pinakion.balanceOf(governor)
