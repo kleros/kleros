14a3eec3b59eeb9216c713eabd16743e0728bb56
epiqueras <epiquerass@gmail.com>
2018-12-11 17:24:53 +0000

    feat(policy-registry): new contract

--- contracts/kleros/PolicyRegistry.sol
+++ contracts/kleros/PolicyRegistry.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.4.24;
 /**
  *  @title PolicyRegistry
  *  @author Enrique Piqueras - <epiquerass@gmail.com>
- *  @dev A contract to maintain a list of all policies for each subcourt.
+ *  @dev A contract to maintain a policy for each subcourt.
  */
 contract PolicyRegistry {
     /* Structs */
@@ -11,27 +11,21 @@ contract PolicyRegistry {
     struct Policy {
         string fileURI;
         bytes32 fileHash;
-        bool deleted;
     }
 
     /* Events */
 
-    /** @dev Emitted when a new policy is created.
+    /** @dev Emitted when a policy is updated.
      *  @param _subcourtID The ID of the policy's subcourt.
-     *  @param _policyID The ID of the policy.
+     *  @param _fileURI The URI to the file containing the policy text.
+     *  @param _fileHash The hash of the file's contents.
      */
-    event PolicyCreation(uint indexed _subcourtID, uint indexed _policyID);
-
-    /** @dev Emitted when a new policy is deleted.
-     *  @param _subcourtID The ID of the policy's subcourt.
-     *  @param _policyID The ID of the policy.
-     */
-    event PolicyDeletion(uint indexed _subcourtID, uint indexed _policyID);
+    event PolicyUpdate(uint indexed _subcourtID, string _fileURI, bytes32 _fileHash);
 
     /* Storage */
 
     address public governor;
-    mapping(uint => Policy[]) internal policies;
+    mapping(uint => Policy) public policies;
 
     /* Modifiers */
 
@@ -40,7 +34,7 @@ contract PolicyRegistry {
 
     /* Constructor */
 
-    /** @dev Constructs the PolicyRegistry contract.
+    /** @dev Constructs the `PolicyRegistry` contract.
      *  @param _governor The governor's address.
      */
     constructor(address _governor) public {governor = _governor;}
@@ -52,41 +46,17 @@ contract PolicyRegistry {
      */
     function changeGovernor(address _governor) external onlyByGovernor {governor = _governor;}
 
-    /** @dev Creates a policy for the specified subcourt.
+    /** @dev Sets the policy for the specified subcourt.
      *  @param _subcourtID The ID of the specified subcourt.
      *  @param _fileURI The URI to the file containing the policy text.
      *  @param _fileHash The hash of the file's contents.
      */
-    function createPolicy(uint _subcourtID, string _fileURI, bytes32 _fileHash) external onlyByGovernor {
-        emit PolicyCreation(
-            _subcourtID,
-            policies[_subcourtID].push(Policy({
-                fileURI: _fileURI,
-                fileHash: _fileHash,
-                deleted: false
-            })) - 1
-        );
-    }
-
-    /** @dev Deletes the specified policy for the specified subcourt.
-     *  @param _subcourtID The ID of the specified subcourt.
-     *  @param _policyID The ID of the specified policy.
-     */
-    function deletePolicy(uint _subcourtID, uint _policyID) external onlyByGovernor {
-        policies[_subcourtID][_policyID].deleted = true;
-        emit PolicyDeletion(_subcourtID, _policyID);
-    }
-
-    /* Public Views */
-
-    /** @dev Gets the specified policy for the specified subcourt.
-     *  @param _subcourtID The ID of the specified subcourt.
-     *  @param _policyID The ID of the specified policy.
-     *  @return The policy.
-     */
-    function policy(uint _subcourtID, uint _policyID) public view returns(string fileURI, bytes32 fileHash, bool deleted) {
-        fileURI = policies[_subcourtID][_policyID].fileURI;
-        fileHash = policies[_subcourtID][_policyID].fileHash;
-        deleted = policies[_subcourtID][_policyID].deleted;
+    function setPolicy(uint _subcourtID, string _fileURI, bytes32 _fileHash) external onlyByGovernor {
+        Policy storage policy = policies[_subcourtID];
+        emit PolicyUpdate(_subcourtID, policy.fileURI, policy.fileHash);
+        policies[_subcourtID] = Policy({
+            fileURI: _fileURI,
+            fileHash: _fileHash
+        });
     }
 }
--- test/kleros/policy-registry.js
+++ test/kleros/policy-registry.js
@@ -2,64 +2,62 @@
 const PolicyRegistry = artifacts.require('./kleros/PolicyRegistry.sol')
 
 contract('PolicyRegistry', accounts =>
-  it('Should let you create and delete subcourt policies.', async () => {
-    // Deploy contract and generate policies
+  it('Should let you set subcourt policies.', async () => {
+    // Deploy contract and generate policies.
     const governor = accounts[0]
+    const subcourtID = 0
     const policyRegistry = await PolicyRegistry.new(governor)
     const policies = [
       {
-        ID: 0,
-        subcourtID: 0,
-        fileURL: 'https://a.b.com',
+        fileURI: 'https://a.b.com',
         fileHash:
           '0x0000000000000000000000000000000000000000000000000000000000000001'
       },
       {
-        ID: 1,
-        subcourtID: 0,
-        fileURL: 'https://c.d.com',
+        fileURI: 'https://c.d.com',
         fileHash:
-          '0x0000000000000000000000000000000000000000000000000000000000000001'
+          '0x0000000000000000000000000000000000000000000000000000000000000002'
       },
       {
-        ID: 0,
-        subcourtID: 1,
-        fileURL: 'https://e.f.com',
+        fileURI: 'https://e.f.com',
         fileHash:
-          '0x0000000000000000000000000000000000000000000000000000000000000001'
+          '0x0000000000000000000000000000000000000000000000000000000000000003'
       },
       {
-        ID: 1,
-        subcourtID: 1,
-        fileURL: 'https://g.h.com',
+        fileURI: 'https://g.h.com',
         fileHash:
-          '0x0000000000000000000000000000000000000000000000000000000000000001'
+          '0x0000000000000000000000000000000000000000000000000000000000000004'
       }
     ]
 
-    // Create policies
+    // Set policies.
     for (const policy of policies)
-      await policyRegistry.createPolicy(
-        policy.subcourtID,
-        policy.fileURL,
+      await policyRegistry.setPolicy(
+        subcourtID,
+        policy.fileURI,
         policy.fileHash
       )
 
-    // Delete every policy and create it again
-    for (const policy of policies) {
-      await policyRegistry.deletePolicy(policy.subcourtID, policy.ID)
-      await policyRegistry.createPolicy(
-        policy.subcourtID,
-        policy.fileURL,
-        policy.fileHash
-      )
-    }
+    // Verify policy update events were emitted.
+    expect(
+      (await new Promise((resolve, reject) =>
+        policyRegistry
+          .PolicyUpdate({ _subcourtID: subcourtID }, { fromBlock: 0 })
+          .get((err, logs) => (err ? reject(err) : resolve(logs)))
+      )).map(e => ({ fileURI: e.args._fileURI, fileHash: e.args._fileHash }))
+    ).to.deep.equal([
+      {
+        fileURI: '',
+        fileHash:
+          '0x0000000000000000000000000000000000000000000000000000000000000000'
+      },
+      ...policies.slice(0, policies.length - 1)
+    ])
 
-    // Verify policies were created correctly
-    for (const policy of policies) {
-      const _policy = await policyRegistry.policy(policy.subcourtID, policy.ID)
-      expect(_policy[0]).to.equal(policy.fileURL)
-      expect(_policy[1]).to.equal(policy.fileHash)
-    }
+    // Verify the last policy is set.
+    const lastPolicy = policies[policies.length - 1]
+    const policy = await policyRegistry.policies(subcourtID)
+    expect(policy[0]).to.equal(lastPolicy.fileURI)
+    expect(policy[1]).to.equal(lastPolicy.fileHash)
   })
 )
