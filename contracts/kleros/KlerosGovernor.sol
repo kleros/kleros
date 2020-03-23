2fd50236a231c17c46c7b6b36b38c998d5463416
Matheus Alencar <matheus@kleros.io>
2020-03-23 17:15:15 +0100

    docs: add reviewer

--- contracts/kleros/KlerosGovernor.sol
+++ contracts/kleros/KlerosGovernor.sol
@@ -1,6 +1,6 @@
 /**
  *  @authors: [@unknownunknown1]
- *  @reviewers: [@ferittuncer, @clesaege]
+ *  @reviewers: [@ferittuncer, @clesaege, @mtsalenc]
  *  @auditors: []
  *  @bounties: []
  *  @deployments: []
