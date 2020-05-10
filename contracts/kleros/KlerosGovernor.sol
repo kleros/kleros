776c2c34d61942c28d0008869f918dc8c4c10c45
Clément Lesaege <clemage@live.fr>
2020-05-10 15:03:45 +0100

    Improve comments.

--- contracts/kleros/KlerosGovernor.sol
+++ contracts/kleros/KlerosGovernor.sol
@@ -27,13 +27,13 @@ contract KlerosGovernor is Arbitrable {
     enum Status {NoDispute, DisputeCreated, Resolved}
 
     struct Session {
-        Round[] rounds; // Tracks each appeal round of a dispute.
+        Round[] rounds; // Tracks each appeal round of the dispute in the session in the form rounds[appeal].
         uint ruling; // The ruling that was given in this session, if any.
         uint disputeID; // ID given to the dispute of the session, if any.
-        uint[] submittedLists; // Tracks all lists that were submitted in a session. submittedLists[submissionID].
+        uint[] submittedLists; // Tracks all lists that were submitted in a session in the form submittedLists[submissionID].
         uint sumDeposit; // Sum of all submission deposits in a session (minus arbitration fees). This is used to calculate the reward.
         Status status; // Status of a session.
-        mapping(bytes32 => bool) alreadySubmitted; // Indicates whether or not the transaction list was already submitted in order to catch duplicates. alreadySubmitted[listHash].
+        mapping(bytes32 => bool) alreadySubmitted; // Indicates whether or not the transaction list was already submitted in order to catch duplicates in the form alreadySubmitted[listHash].
         uint durationOffset; // Time in seconds that prolongs the submission period after the first submission, to give other submitters time to react.
     }
 
@@ -372,6 +372,7 @@ contract KlerosGovernor is Arbitrable {
         reservedETH += contribution;
 
         if (shadowWinner != NO_SHADOW_WINNER && shadowWinner != _submissionID && round.hasPaid[_submissionID]) {
+            // Two sides are fully funded.
             shadowWinner = NO_SHADOW_WINNER;
             arbitrator.appeal.value(appealCost)(session.disputeID, arbitratorExtraData);
             session.rounds.length++;
