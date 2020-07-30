c76f9d1613cf9ebf87b17208e8b4752f43ca8414
Harsh Gupta <hgupta@ma.iitr.ac.in>
2020-07-31 05:10:39 +0530

    Update PolicyRegistry.sol

--- contracts/kleros/PolicyRegistry.sol
+++ contracts/kleros/PolicyRegistry.sol
@@ -6,7 +6,9 @@ pragma solidity ^0.4.24;
  *  @dev A contract to maintain a policy for each subcourt.
  */
 contract PolicyRegistry {
-    /* Events */
+    /***** *****/
+    /*  Events */
+    /***** *****/
 
     /** @dev Emitted when a policy is updated.
      *  @param _subcourtID The ID of the policy's subcourt.
@@ -14,24 +16,38 @@ contract PolicyRegistry {
      */
     event PolicyUpdate(uint indexed _subcourtID, string _policy);
 
-    /* Storage */
+    
+    /***** ******/
+    /*  Storage */
+    /***** ******/
+
 
     address public governor;
     mapping(uint => string) public policies;
 
-    /* Modifiers */
+ 
+    /***** ********/
+    /*  Modifiers */
+    /***** ********/
 
     /** @dev Requires that the sender is the governor. */
     modifier onlyByGovernor() {require(governor == msg.sender, "Can only be called by the governor."); _;}
 
+    
+    
+    /****** ********/
     /* Constructor */
+    /****** ********/
+
 
     /** @dev Constructs the `PolicyRegistry` contract.
      *  @param _governor The governor's address.
      */
     constructor(address _governor) public {governor = _governor;}
 
+    /****** *****/
     /* External */
+    /****** *****/
 
     /** @dev Changes the `governor` storage variable.
      *  @param _governor The new value for the `governor` storage variable.
