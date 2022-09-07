f3e65ad0153e6f7230c2036ed2fe7e4f8cd3871d
Clément Lesaege <clesaege@users.noreply.github.com>
2022-09-07 08:10:03 +0000

    Update Migrations.sol

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.4.4;
 
-// Test Imports
+// Test Imports.
 import "@kleros/kleros-interaction/contracts/standard/rng/ConstantNG.sol";
 import "@kleros/kleros-interaction/contracts/standard/arbitration/EnhancedAppealableArbitrator.sol";
 import "@kleros/kleros-interaction/contracts/standard/arbitration/TwoPartyArbitrable.sol";
