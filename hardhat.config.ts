5e002b2d3ae9ac98d467c3a30ab1d0c0cc9889a0
gratestas <stasgrate@gmail.com>
2022-11-16 14:53:35 +0300

    chore: configure hardhat

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,7 +1,36 @@
 import { HardhatUserConfig } from 'hardhat/types';
+import 'hardhat-deploy';
+import 'hardhat-deploy-ethers';
+import '@typechain/hardhat';
+
 const config: HardhatUserConfig = {
   solidity: {
-    version: '0.4.24',
+    compilers: [
+      {
+        version: '0.4.24',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 100,
+          },
+        },
+      },
+      {
+        version: '0.4.26',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 100,
+          },
+        },
+      },
+    ],
+  },
+  namedAccounts: {
+    deployer: 0,
+  },
+  paths: {
+    sources: 'src',
   },
 };
 export default config;
