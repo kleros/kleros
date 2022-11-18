6e6488964c4cca0d404be3c6594689edb17132a7
gratestas <stasgrate@gmail.com>
2022-11-18 10:52:36 +0300

    feat(deploy): add extra views script

---
+++ deploy/03-deploy-extra-views.ts
@@ -0,0 +1,24 @@
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+import { DeployFunction } from 'hardhat-deploy/types';
+
+const deployKlerosLiquidExtraViews: DeployFunction = async function(
+  hre: HardhatRuntimeEnvironment
+) {
+  const { deployments, getNamedAccounts } = hre;
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
+
+  const KlerosLiquid = await deployments.get(
+    'KlerosLiquid'
+  );
+
+  await deploy('KlerosLiquidExtraViews', {
+    from: deployer,
+    args: [KlerosLiquid.address],
+    log: true,
+  });
+};
+
+deployKlerosLiquidExtraViews.tags = ['KlerosLiquidExtraViews'];
+deployKlerosLiquidExtraViews.dependencies = ['KlerosLiquid'];
+export default deployKlerosLiquidExtraViews;
