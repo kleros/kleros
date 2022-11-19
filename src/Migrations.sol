24cd19bfde4191198b6ea302c19d154677d108f5
gratestas <stasgrate@gmail.com>
2022-11-19 16:49:22 +0300

    fix: bring back deleted migration contract

---
+++ src/Migrations.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.4.4;
+
+// Test Imports.
+import "@kleros/kleros-interaction/contracts/standard/rng/ConstantNG.sol";
+import "@kleros/kleros-interaction/contracts/standard/arbitration/EnhancedAppealableArbitrator.sol";
+import "@kleros/kleros-interaction/contracts/standard/arbitration/TwoPartyArbitrable.sol";
+
+contract Migrations {
+    address public owner;
+    uint public last_completed_migration;
+
+    modifier isOwner() {
+        if (msg.sender == owner) _;
+    }
+
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    function setCompleted(uint completed) public isOwner {
+        last_completed_migration = completed;
+    }
+
+    function upgrade(address newAddress) public isOwner {
+        Migrations upgraded = Migrations(newAddress);
+        upgraded.setCompleted(last_completed_migration);
+    }
+}
