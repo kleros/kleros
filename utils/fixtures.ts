c6e3080a85aee94560e893b73b70b1bc287be63a
gratestas <stasgrate@gmail.com>
2022-11-21 21:30:04 +0300

    test(utils): add setup fixture for tx execution

--- utils/fixtures.ts
+++ utils/fixtures.ts
@@ -3,7 +3,10 @@ import {
   EnhancedAppealableArbitrator,
   KlerosGovernor,
 } from '../typechain-types';
+import { PromiseOrValue } from '../typechain-types/common';
 import { increaseTime } from './test-helpers';
+import { BytesLike } from 'ethers';
+import { soliditySha3 } from 'web3-utils';
 
 export const useInitialSetup = deployments.createFixture(async ({ ethers }) => {
   const [
@@ -116,8 +119,77 @@ export const useRulingSetup = deployments.createFixture(async () => {
   await increaseTime(args.submissionTimeout + 1);
   await governor.connect(users.deployer).executeSubmissions();
 
-  // Ruling 1 is equal to 0 submission index (submitter1)
-  await appeableArbitrator.giveRuling(0, 1);
-
   return { governor, appeableArbitrator, users, args };
 });
+
+export const useTransactionsSetup = deployments.createFixture(
+  async ({ ethers }) => {
+    const {
+      governor,
+      appeableArbitrator,
+      args,
+      users,
+    } = await useInitialSetup();
+
+    const listDescription = 'tx1, tx2, tx3';
+    let index1: number;
+    let index2: number;
+    let dataString: PromiseOrValue<BytesLike>;
+
+    const addresses = [appeableArbitrator.address, governor.address];
+    const values = [ethers.utils.parseEther('0.1'), 0];
+    const data = [101, 36];
+
+    const txHash1 = parseInt(
+      soliditySha3(
+        appeableArbitrator.address,
+        ethers.utils.parseEther('0.1').toString(),
+        '0xc13517e1000000000000000000000000000000000000000000000000000000000000000b00000000000000000000000000000000000000000000000000000000000000400000000000000000000000000000000000000000000000000000000000000001fa'
+      ) as string,
+      16
+    );
+    const txHash2 = parseInt(
+      soliditySha3(
+        governor.address,
+        0,
+        '0x246c76df0000000000000000000000000000000000000000000000000000000000000014'
+      ) as string,
+      16
+    );
+
+    if (txHash1 < txHash2) {
+      index1 = 0;
+      index2 = 1;
+      dataString =
+        '0xc13517e1000000000000000000000000000000000000000000000000000000000000000b00000000000000000000000000000000000000000000000000000000000000400000000000000000000000000000000000000000000000000000000000000001fa246c76df0000000000000000000000000000000000000000000000000000000000000014';
+    } else {
+      index1 = 1;
+      index2 = 0;
+      dataString =
+        '0x246c76df0000000000000000000000000000000000000000000000000000000000000014c13517e1000000000000000000000000000000000000000000000000000000000000000b00000000000000000000000000000000000000000000000000000000000000400000000000000000000000000000000000000000000000000000000000000001fa';
+    }
+
+    await governor
+      .connect(users.submitter1)
+      .submitList(
+        [addresses[index1], addresses[index2]],
+        [values[index1], values[index2]],
+        dataString,
+        [data[index1], data[index2]],
+        listDescription,
+        { value: args.submissionDeposit() }
+      );
+
+    await increaseTime(args.submissionTimeout + 1);
+
+    await governor.connect(users.deployer).executeSubmissions();
+
+    // Send spendable money via fallback.
+    await users.other.sendTransaction({
+      to: governor.address,
+      value: ethers.utils.parseEther('3'),
+    });
+
+    return { governor, appeableArbitrator, users, args };
+  }
+);
