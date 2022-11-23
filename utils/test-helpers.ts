340873b7d0ff04649e14d9ede2246586b7708fa8
gratestas <stasgrate@gmail.com>
2022-11-23 16:19:46 +0300

    chore(test): add test helpers

--- utils/test-helpers.ts
+++ utils/test-helpers.ts
@@ -1,4 +1,6 @@
+import { BigNumber, BigNumberish, ContractFunction } from 'ethers';
 import { ethers } from 'hardhat';
+import { PromiseOrValue } from '../typechain-types/common';
 
 export const getCurrentTimestamp = async () =>
   (await ethers.provider.getBlock(await ethers.provider.getBlockNumber()))
@@ -8,3 +10,81 @@ export const increaseTime = async (amount: number) =>
   await ethers.provider.send('evm_mine', [
     (await getCurrentTimestamp()) + amount,
   ]);
+
+export const asyncForEach = async function<F extends ContractFunction>(
+  method: F,
+  iterable: { [s: string]: unknown } | ArrayLike<unknown>
+): Promise<void> {
+  const array = Array.isArray(iterable) ? iterable : Object.values(iterable);
+  for (const arg of array) await method(arg);
+};
+
+const randomInt = (max: number, min = 1) =>
+  Math.max(min, Math.ceil(Math.random() * max));
+
+interface FixedLengthArray<T, L extends number> extends ArrayLike<T> {
+  length: L;
+}
+export const generateSubcourts = (
+  depth: number,
+  ID = 0,
+  args: {
+    minStake: number;
+    alpha: number;
+    feeForJuror: BigNumber | number;
+    jurorsForCourtJump: number;
+    timesPerPeriod: PromiseOrValue<BigNumberish>[];
+    sortitionSumTreeK: number;
+  },
+  subcourtMap = {}
+) => {
+  const {
+    minStake,
+    alpha,
+    feeForJuror,
+    jurorsForCourtJump,
+    timesPerPeriod,
+    sortitionSumTreeK,
+  } = args;
+  const newMinStake = Math.max(randomInt(minStake + 100), minStake);
+  const subcourtTree = {
+    ID,
+    children:
+      depth > 1
+        ? [...new Array(sortitionSumTreeK)].map(
+            (_, i) =>
+              generateSubcourts(
+                depth - 1,
+                sortitionSumTreeK * ID + i + 1,
+                {
+                  minStake: newMinStake,
+                  alpha,
+                  feeForJuror,
+                  jurorsForCourtJump,
+                  timesPerPeriod,
+                  sortitionSumTreeK,
+                },
+                subcourtMap
+              ).subcourtTree
+          )
+        : undefined,
+    hiddenVotes: ID % 2 === 0,
+    minStake: newMinStake,
+    alpha,
+    feeForJuror,
+    jurorsForCourtJump,
+    timesPerPeriod,
+    sortitionSumTreeK,
+  };
+  if (ID === 0) subcourtTree.parent = 0;
+  else {
+    subcourtTree.parent = Math.floor((ID - 1) / sortitionSumTreeK);
+    subcourtMap[subcourtTree.ID] = {
+      ...subcourtTree,
+      children:
+        subcourtTree.children &&
+        subcourtTree.children.map((child: { ID: any }) => child.ID),
+    };
+  }
+  return { subcourtMap, subcourtTree };
+};
