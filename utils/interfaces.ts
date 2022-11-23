9b7b54e15b764618a477dbec3073bb1918352add
gratestas <stasgrate@gmail.com>
2022-11-23 16:21:24 +0300

    chore(test): add interfaces

--- utils/interfaces.ts
+++ utils/interfaces.ts
@@ -1,8 +1,30 @@
-import { BigNumber } from 'ethers';
+import { BigNumber, BigNumberish } from 'ethers';
+import { PromiseOrValue } from '../typechain-types/common';
 
 export interface TransactionInfo {
   target: string;
   value: BigNumber;
   data: string;
   executed: boolean;
+}
+
+export interface SubcourtInfo {
+  ID: PromiseOrValue<BigNumberish>;
+  parent: PromiseOrValue<BigNumberish>;
+  hiddenVotes: boolean | Promise<boolean>;
+  minStake: PromiseOrValue<BigNumberish>;
+  alpha: PromiseOrValue<BigNumberish>;
+  feeForJuror: PromiseOrValue<BigNumberish>;
+  jurorsForCourtJump: PromiseOrValue<BigNumberish>;
+  timesPerPeriod: [
+    PromiseOrValue<BigNumberish>,
+    PromiseOrValue<BigNumberish>,
+    PromiseOrValue<BigNumberish>,
+    PromiseOrValue<BigNumberish>
+  ];
+  sortitionSumTreeK: PromiseOrValue<BigNumberish>;
+}
+
+export interface FixedLengthArray<T, L extends number> extends ArrayLike<T> {
+  length: L;
 }
