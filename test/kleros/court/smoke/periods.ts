512d2619cbfa1a1b691b033945c6f55a4be5884e
gratestas <stasgrate@gmail.com>
2022-11-28 09:28:05 +0300

    chore: rename test files

---
+++ test/kleros/court/smoke/periods.ts
@@ -0,0 +1,112 @@
+import { expect } from 'chai';
+import { ethers } from 'hardhat';
+
+import { useDisputeSetup, useStakedSetup } from 'utils/fixtures/kleros-liquid';
+import { Period } from 'utils/index';
+import { getVoteIDs, increaseTime } from 'utils/test-helpers';
+
+const NUMBER_OF_CHOICES = 2;
+
+describe('Smoke: Dispute - Peroids', () => {
+  const disputeID = 0;
+  it('Should pass periods of dispute', async () => {
+    const numberOfJurors = 1;
+    const { klerosLiquid } = await useStakedSetup(numberOfJurors);
+
+    await klerosLiquid.drawJurors(disputeID, 3);
+
+    await expect(klerosLiquid.passPeriod(disputeID))
+      .to.emit(klerosLiquid, 'NewPeriod')
+      .withArgs(disputeID, Period.vote);
+
+    let dispute = await klerosLiquid.disputes(disputeID);
+    expect(dispute.period).to.be.equal(Period.vote);
+
+    await klerosLiquid.castVote(disputeID, [0], NUMBER_OF_CHOICES, 0);
+
+    await expect(klerosLiquid.passPeriod(disputeID))
+      .to.emit(klerosLiquid, 'NewPeriod')
+      .withArgs(disputeID, Period.appeal);
+
+    dispute = await klerosLiquid.disputes(disputeID);
+    expect(dispute.period).to.be.equal(Period.appeal);
+
+    const appealPeriod = await klerosLiquid.appealPeriod(disputeID);
+    await increaseTime(appealPeriod.end.toNumber());
+
+    await expect(klerosLiquid.passPeriod(disputeID))
+      .to.emit(klerosLiquid, 'NewPeriod')
+      .withArgs(disputeID, Period.execution);
+
+    dispute = await klerosLiquid.disputes(disputeID);
+    expect(dispute.period).to.be.equal(Period.execution);
+  });
+
+  describe('Revert Execution', () => {
+    const numberOfJurors = 3;
+    it('Should fail to pass Evidence Period: period time has not passed', async () => {
+      const { klerosLiquid } = await useDisputeSetup(numberOfJurors);
+
+      await expect(klerosLiquid.passPeriod(disputeID)).to.be.revertedWith(
+        'The evidence period time has not passed yet and it is not an appeal.'
+      );
+    });
+
+    it('Should fail to pass Evidence Period: not enough juror drawn', async () => {
+      const { klerosLiquid } = await useStakedSetup(numberOfJurors);
+
+      await klerosLiquid.drawJurors(disputeID, numberOfJurors - 1);
+
+      await expect(klerosLiquid.passPeriod(disputeID)).to.be.revertedWith(
+        'The dispute has not finished drawing yet.'
+      );
+    });
+
+    it('Should fail to pass Voting Period: not all jurors voted', async () => {
+      const { klerosLiquid, jurors } = await useStakedSetup(numberOfJurors);
+
+      const tx = await klerosLiquid.drawJurors(disputeID, 10);
+      await klerosLiquid.passPeriod(disputeID);
+
+      const voteIDs = await getVoteIDs(tx);
+
+      const voteId = Number(voteIDs.get(jurors[0].address));
+      await klerosLiquid.castVote(disputeID, [voteId], NUMBER_OF_CHOICES, 0);
+
+      await expect(klerosLiquid.passPeriod(disputeID)).to.be.revertedWith(
+        'The vote period time has not passed yet and not every juror has voted yet.'
+      );
+    });
+
+    it('Should fail to pass Appeal and Execution periods', async () => {
+      const { klerosLiquid, subcourt, jurors } = await useStakedSetup(
+        numberOfJurors
+      );
+
+      const tx = await klerosLiquid.drawJurors(disputeID, 10);
+      await klerosLiquid.passPeriod(disputeID);
+
+      const voteIDs = await getVoteIDs(tx);
+
+      for (const juror of Object.values(jurors)) {
+        const voteId = Number(voteIDs.get(juror.address));
+        if (voteIDs.has(juror.address))
+          await klerosLiquid
+            .connect(juror)
+            .castVote(disputeID, [voteId], NUMBER_OF_CHOICES, 0);
+      }
+
+      await klerosLiquid.passPeriod(disputeID);
+      await expect(klerosLiquid.passPeriod(disputeID)).to.be.revertedWith(
+        'The appeal period time has not passed yet.'
+      );
+
+      await increaseTime(subcourt.timesPerPeriod[Period.appeal]);
+      await klerosLiquid.passPeriod(disputeID);
+
+      await expect(klerosLiquid.passPeriod(disputeID)).to.be.revertedWith(
+        'The dispute is already in the last period.'
+      );
+    });
+  });
+});
---
+++ test/kleros/court/smoke/voting.ts
@@ -0,0 +1,98 @@
+import { expect } from 'chai';
+import { ethers } from 'hardhat';
+
+import { useStakedSetup } from 'utils/fixtures/kleros-liquid';
+import { getVoteIDs } from 'utils/test-helpers';
+
+describe('Smoke: Dispute - Voting', () => {
+  const disputeID = 0;
+  it('Should validate coherent voting', async () => {
+    const numberOfJurors = 3;
+    const coherentChoice = 2;
+    const { klerosLiquid, jurors } = await useStakedSetup(numberOfJurors);
+
+    const tx = await klerosLiquid.drawJurors(disputeID, 6);
+    await klerosLiquid.passPeriod(disputeID);
+
+    const voteIDs = await getVoteIDs(tx);
+
+    for (const juror of Object.values(jurors)) {
+      const voteId = Number(voteIDs.get(juror.address));
+      if (voteIDs.has(juror.address))
+        await klerosLiquid
+          .connect(juror)
+          .castVote(disputeID, [voteId], coherentChoice, 0);
+    }
+
+    const voteCount = await klerosLiquid.getVoteCounter(disputeID, 0);
+    expect(voteCount.counts[coherentChoice]).to.be.equal(voteIDs.size);
+    expect(voteCount.winningChoice).to.be.equal(coherentChoice);
+    expect(voteCount.tied).to.be.equal(false);
+
+    voteIDs.forEach(async (vid, juror) => {
+      const vote = await klerosLiquid.getVote(disputeID, 0, vid);
+
+      expect(vote.account).to.be.equal(juror);
+      expect(vote.choice).to.be.equal(coherentChoice);
+      expect(vote.commit).to.be.equal(ethers.constants.HashZero);
+      expect(vote.voted).to.be.equal(true);
+    });
+  });
+
+  it('Should validate tied voting', async () => {
+    const numberOfJurors = 2;
+    const choices = new Map<string, number>();
+    const { klerosLiquid, jurors } = await useStakedSetup(numberOfJurors);
+
+    const tx = await klerosLiquid.drawJurors(disputeID, 6);
+    await klerosLiquid.passPeriod(disputeID);
+
+    const voteIDs = await getVoteIDs(tx);
+
+    let index = 0;
+    voteIDs.forEach((_, juror) => choices.set(juror, index++));
+
+    for (const juror of Object.values(jurors)) {
+      const voteId = Number(voteIDs.get(juror.address));
+      const choice = Number(choices.get(juror.address));
+
+      if (voteIDs.has(juror.address))
+        await klerosLiquid
+          .connect(juror)
+          .castVote(disputeID, [voteId], choice, 0);
+    }
+
+    const voteCount = await klerosLiquid.getVoteCounter(disputeID, 0);
+    expect(voteCount.tied).to.be.equal(true);
+  });
+
+  describe('Revert Execution', () => {
+    it('Should fail voting if any requirement is unmet', async () => {
+      const numberOfJurors = 1;
+      const { klerosLiquid, jurors } = await useStakedSetup(numberOfJurors);
+
+      await klerosLiquid.drawJurors(disputeID, 6);
+      await klerosLiquid.passPeriod(disputeID);
+
+      await expect(
+        klerosLiquid.castVote(disputeID, [], 1, 0),
+        'Vote ID array cannot be empty'
+      ).to.be.reverted;
+
+      await expect(
+        klerosLiquid.castVote(disputeID, [0], 3, 0)
+      ).to.be.revertedWith(
+        'The choice has to be less than or equal to the number of choices for the dispute.'
+      );
+
+      await klerosLiquid.castVote(disputeID, [0], 1, 0);
+      await expect(
+        klerosLiquid.castVote(disputeID, [0], 1, 0)
+      ).to.be.revertedWith('Vote already cast.');
+
+      await expect(
+        klerosLiquid.connect(jurors[1]).castVote(disputeID, [0], 1, 0)
+      ).to.be.revertedWith('The caller has to own the vote.');
+    });
+  });
+});
